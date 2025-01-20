6ea78eb27f02173a4d43f95a0439498b1a7afc85
joaocandidozup <joao.candido@zup.com>
2025-01-20 11:59:05 -0300

    refactor: add method to check if the email is already registered

--- src/main/java/com/zup/pizzaria/repository/ClienteRepository.java
+++ src/main/java/com/zup/pizzaria/repository/ClienteRepository.java
@@ -4,4 +4,7 @@ import com.zup.pizzaria.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    boolean existsByEmail(String email);
 }
+
