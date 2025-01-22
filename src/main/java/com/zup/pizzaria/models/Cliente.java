697c70a769ab6648584b2bf4de461c47d6bae976
joaocandidozup <joao.candido@zup.com>
2025-01-22 11:44:57 -0300

    chore: remove unnecessary whitespace from code

--- src/main/java/com/zup/pizzaria/models/Cliente.java
+++ src/main/java/com/zup/pizzaria/models/Cliente.java
@@ -8,13 +8,9 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
-
     private String email;
-
     private String telefone;
-
 
     public Cliente() {
     }
@@ -23,23 +19,19 @@ public class Cliente {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
-    }
 
+    }
 
     public Long getId() {
         return id;
     }
-
     public String getNome() {
         return nome;
     }
-
     public String getEmail() {
         return email;
     }
-
     public String getTelefone() {
         return telefone;
     }
 }
-
